334d1d9556f1badacb9cee4a6fb97165a495fca3
Alex McVittie <alexander.mcvittie@gmail.com>
2016-09-17 01:32:20 -0400

    Incremental changes Just a few things. All still working

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Licensed under GNU GPLv3
 
 
 # Import local libraries
-from api_key import api 
+import api_key as api
 
 # Import external libraries
 import googlemaps
@@ -22,5 +22,16 @@ gmapobj = googlemaps.Client(key=google_api_key)
 directions = gmapobj.directions("511 albert street, waterloo ON", 
                                 "200 University Avenue, Waterloo ON",
                                 mode="driving")
-    
 
+# Data dictionary is stored in the first element of the list
+directions_dict = directions[0]
+
+journey_legs = directions_dict["legs"][0]
+print type(journey_legs)
+print journey_legs.keys()
+
+trip_steps = journey_legs['steps']
+print (trip_steps)
+
+
+# End of file
