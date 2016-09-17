766301fa7fe58e5958fb09c4cb46abc1cd55cef5
Alex McVittie <alexander.mcvittie@gmail.com>
2016-09-17 06:46:22 -0400

    Exisiting hour markers are determined Next up - interpolating route locations.

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ import api_key as api
 import googlemaps
 
 # Initialize variables that are to be accessed from multiple functions
-total_traveltime = 0
-
 # Contains the time, weather and location for each waypoint 
 # Format:{ NumMins:{"weather":WeatherObj, "lat":Latitude, "lon":Longitude}}
 twl_dict = {} 
@@ -26,45 +24,40 @@ google_api_key = api.directions_api_key
 # Create a GoogleMapss object using the API key
 gmapobj = googlemaps.Client(key=google_api_key)
 
-def sum_time(leg_time, lat, lon):
+def sum_time(leg_time, lat, lon, total_traveltime):
     global twl_dict
-    global total_traveltime
     # Convert leg_time to minutes
     leg_time = leg_time / 60.0
     
     # Flag that is triggered if the current leg time brings it close enough to
     # be at the next hour of the journey
     flag = False
-    # Calculate number of hours before adding current leg
-    num_hrs_before = int(total_traveltime) / 60
-    num_mins_before = int(total_traveltime) % 60 
-    
-    num_mins_after = num_mins_before + leg_time
-    total_traveltime += leg_time
-    if num_mins_after > 50:
-        flag = True 
-        twl_dict[num_hrs_before + 1] = {"lat":lat, "lon":lon}
-        print lat
-        print lon
-        print num_hrs_before + 1
-        print ""
-        print ""
-        
-    
+    hours_before = int(total_traveltime) / 60
 
+    total_traveltime += leg_time
+    hours_now = int(total_traveltime) / 60
+
+    if total_traveltime % 60 > 50:
+        total_traveltime = (hours_now + 1) * 60
+        print total_traveltime
+
+
+    return total_traveltime
 def calc_directions(start, end, dir_mode):
     directions = gmapobj.directions(start, end,  mode=dir_mode)
     # Data dictionary is stored in the first element of the list
     directions_dict = directions[0]
     print len(directions_dict["legs"])
     journey_legs = directions_dict["legs"][0]
-
+    total_traveltime = 0 
     # Iterate through the steps
     for step in journey_legs['steps']:
         # print (step.keys())
-        sum_time(step["duration"]["value"],
-                 step["end_location"]["lat"],
-                 step["end_location"]["lng"])
+        # print total_traveltime
+        total_traveltime = sum_time(step["duration"]["value"],
+                                    step["end_location"]["lat"],
+                                    step["end_location"]["lng"],
+                                    total_traveltime)
         
 
 # Sample calc_directions method call - also for testing
